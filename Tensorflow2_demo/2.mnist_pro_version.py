3892d7b5cbb42f2f1d6bab3766c31254dc8953a5
liuyf <lyfflyl@gmail.com>
2019-11-04 19:54:06 +0800

    add yolo keras code

--- Tensorflow2_demo/2.mnist_pro_version.py
+++ Tensorflow2_demo/2.mnist_pro_version.py
@@ -24,13 +24,15 @@ test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 class MyModel(Model):
     def __init__(self):
         super(MyModel,self).__init__()
-        self.conv1 = Conv2D(32,3,activation='relu')
+        self.conv1 = Conv2D(32,3,activation='relu')        
+        self.conv2 = Conv2D(64,3,activation='relu')
         self.flatten = Flatten()
         self.d1 = Dense(128,activation='relu')
         self.d2 = Dense(10,activation='softmax')
 
     def call(self,x):
         x = self.conv1(x)
+        x = self.conv2(x)        
         x = self.flatten(x)
         x = self.d1(x)
         return self.d2(x)
